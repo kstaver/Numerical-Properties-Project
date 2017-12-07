88fea7abc7348f65eb3d4993e5f616e638ff6500
EspurrBuns <icanhazmaths@gmail.com>
2017-12-06 18:07:38 -0800

    Defined main function for determining numerical properties.

--- base.py
+++ base.py
@@ -4,20 +4,26 @@
 print('Please input a number.')
 
 num = input()
-
 num = int(num)
 
-if num % 4 == 0:
-    print (num, 'is a multiple of 4. Therefore it is even by definition.')
-elif num % 2 == 0:
-    myList = []
-    for i in range(1, num + 1):
-        if num % i == 0:
-            myList.append(i)
-    print (num, 'is an even number. Its factors are', myList,'.')
-else:
-    myList = []
-    for i in range(1, num + 1):
-        if num % i == 0:
-            myList.append(i)
-    print(num, 'is not an even number. Its factors are',myList,'.')
+def main(num):
+    if num % 4 == 0:
+        print (num, 'is a multiple of 4. Therefore it is even by definition.')
+    elif num % 2 == 0:
+        myList = []
+        for i in range(1, num + 1):
+            if num % i == 0:
+                myList.append(i)
+        print (num, 'is an even number. Its factors are', myList,'.')
+
+    else:
+        myList = []
+        for i in range(1, num + 1):
+            if num % i == 0:
+                myList.append(i)
+        print(num, 'is not an even number. Its factors are',myList,'.')
+
+main(num)
+
+#function for menu
+#function for exit??
